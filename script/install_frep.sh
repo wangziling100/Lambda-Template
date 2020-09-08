e7f830703d57f4684eafb37ff0ef30344b81fc80
Bob Wang <wangziling1000@gmail.com>
2020-09-08 18:10:54 +0200

    fix: update install frep script

--- script/install_frep.sh
+++ script/install_frep.sh
@@ -1,2 +1,2 @@
-curl -fSL https://github.com/subchen/frep/releases/download/v1.3.10/frep-1.3.10-linux-amd64 -o /usr/local/bin/frep
-chmod +x /usr/local/bin/frep
+curl -fSL https://github.com/subchen/frep/releases/download/v1.3.10/frep-1.3.10-linux-amd64 -o ./frep
+chmod +x frep
