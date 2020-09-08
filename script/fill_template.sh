65dbd328d2bf586973ff566e70970178a56aa9db
Bob Wang <wangziling1000@gmail.com>
2020-09-08 20:10:54 +0200

    fix: update template

--- script/fill_template.sh
+++ script/fill_template.sh
@@ -1,7 +1,15 @@
 old_dir=`pwd`
 cd $(dirname $0)
-frep ../template.yaml.template:../template.yaml --load ../config.yaml --overwrite
-frep ../samconfig.toml.template:../samconfig.toml --load ../config.yaml --overwrite
-frep ../env.json.template:../env.json --load ../config.yaml --overwrite
-frep ../autoCI.config.json.template:../.github/autoCI.config.json --load ../config.yaml --overwrite
+if [[ "$BUILD_ENV" == 'ci' ]]; then
+    ./frep ../template.yaml.template:../template.yaml --load ../config.yaml --overwrite
+    ./frep ../samconfig.toml.template:../samconfig.toml --load ../config.yaml --overwrite
+    ./frep ../env.json.template:../env.json --load ../config.yaml --overwrite
+    ./frep ../autoCI.config.json.template:../.github/autoCI.config.json --load ../config.yaml --overwrite
+else
+    frep ../template.yaml.template:../template.yaml --load ../config.yaml --overwrite
+    frep ../samconfig.toml.template:../samconfig.toml --load ../config.yaml --overwrite
+    frep ../env.json.template:../env.json --load ../config.yaml --overwrite
+    frep ../autoCI.config.json.template:../.github/autoCI.config.json --load ../config.yaml --overwrite
+fi
+
 cd $old_dir
