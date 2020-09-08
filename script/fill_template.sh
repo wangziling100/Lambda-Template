0a66f83299adfe05589cb9dd0f7b072d264ae074
Bob Wang <wangziling1000@gmail.com>
2020-09-08 20:03:53 +0200

    fix: update template

--- script/fill_template.sh
+++ script/fill_template.sh
@@ -1,7 +1,7 @@
 old_dir=`pwd`
 cd $(dirname $0)
-../frep ../template.yaml.template:../template.yaml --load ../config.yaml --overwrite
-../frep ../samconfig.toml.template:../samconfig.toml --load ../config.yaml --overwrite
-../frep ../env.json.template:../env.json --load ../config.yaml --overwrite
-../frep ../autoCI.config.json.template:../.github/autoCI.config.json --load ../config.yaml --overwrite
+frep ../template.yaml.template:../template.yaml --load ../config.yaml --overwrite
+frep ../samconfig.toml.template:../samconfig.toml --load ../config.yaml --overwrite
+frep ../env.json.template:../env.json --load ../config.yaml --overwrite
+frep ../autoCI.config.json.template:../.github/autoCI.config.json --load ../config.yaml --overwrite
 cd $old_dir
--- script/install_frep.sh
+++ script/install_frep.sh
@@ -1,2 +1,2 @@
-curl -fSL https://github.com/subchen/frep/releases/download/v1.3.10/frep-1.3.10-linux-amd64 -o ./frep
-chmod +x frep
+curl -fSL https://github.com/subchen/frep/releases/download/v1.3.10/frep-1.3.10-linux-amd64 -o ./script/frep
+chmod +x ./script/frep
